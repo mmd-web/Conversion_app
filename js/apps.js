a61a591b139af2776e8a91062aab3a00d308bb88
78 mmd 69 <mmdqurani@gmail.com>
2024-04-02 21:33:28 +0330

    Connecting document elements to JS

---
+++ js/apps.js
@@ -0,0 +1,12 @@
+let $ = document;
+
+let inputBox = $.getElementById ("converter");
+let descriptionText = $.getElementById ("Description");
+let degreesF = $.getElementById ("F");
+let degreesC = $.getElementById ("C");
+let errorText = $.getElementById ("error_text");
+let outputText = $ .getElementById ("output");
+let changeBtn = $.getElementById ("change");
+let resetBtn = $.getElementById ("reset");
+let convertBtn = $.getElementById ("convert");
+
